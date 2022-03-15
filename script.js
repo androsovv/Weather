69e25f9d63eb375f333614d14eae8eccd27cfb5e
homePC <vladimirandrosov13@gmail.com>
2022-03-15 23:40:49 +0300

    Added working favorite button

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
  const UI = {
-    TABS_CONTENT: document.querySelectorAll('.box__left__tabs__item'),
-    TABS_BTN: document.querySelectorAll('.box__left__options__item'),
-    TABS_PARENT: document.querySelector('.box__left__options'),
-    SEARCH_TOWN: document.querySelector('.search__town'),
-    SEARCH_BTN: document.querySelector('.search__img'),
-    TEMPERATURE_NOW: document.querySelector('.temperature__now'),
-    TOWN_NAME: document.querySelectorAll('.town__name'),
-
-
+   TABS_CONTENT: document.querySelectorAll('.box__left__tabs__item'),
+   TABS_BTN: document.querySelectorAll('.box__left__options__item'),
+   TABS_PARENT: document.querySelector('.box__left__options'),
+   SEARCH_TOWN: document.querySelector('.search__town'),
+   SEARCH_BTN: document.querySelector('.search__img'),
+   TEMPERATURE_NOW: document.querySelector('.temperature__now'),
+   TOWN_NAME: document.querySelectorAll('.town__name'),
+   ADD_FAVORITE: document.querySelector('.box__left_bottom__favorite'),
+   TOWNS_PARENT: document.querySelector('.box__right__towns'),
 
 
  };
+
+ let favoriteTowns = [];
 
 
 
@@ -50,31 +52,54 @@
  });
 
  UI.SEARCH_BTN.addEventListener('click', event => {
-
     event.preventDefault();
-
     const serverUrl = 'http://api.openweathermap.org/data/2.5/weather',
-       cityName = UI.SEARCH_TOWN.value,
-       apiKey = 'f660a2fb1e4bad108d6160b7f58c555f',
-       url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+          cityName = UI.SEARCH_TOWN.value,
+          apiKey = 'f660a2fb1e4bad108d6160b7f58c555f',
+          url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
 
     let weatherResponse = fetch(url);
 
     weatherResponse.then(getWheather => getWheather.json())
        .then(result => {
           console.log(result);
-          console.log(typeof result.weather);
-
+          UI.SEARCH_TOWN.value = '';
           UI.TEMPERATURE_NOW.textContent = convertTemperature(result.main.temp) + '°';
           UI.TOWN_NAME.forEach(item => {
              item.textContent = result.name;
           });
-
        })
 
-
+       
        .catch(error => alert(error));
  });
+
+UI.ADD_FAVORITE.addEventListener('click', event => {
+   event.preventDefault();
+   let townName = document.querySelector('.town__name').textContent;
+   console.log(favoriteTowns);
+
+   if (favoriteTowns.length === 0) {
+      const newFavoriteTown = document.createElement('div');
+         newFavoriteTown.classList = 'box__right__towns__item';
+         newFavoriteTown.innerHTML = `<div class="box__right__towns__item__name">${townName}</div>
+         <img src="srs/icons/remove-icon.svg" alt="" class="delete">`;
+         UI.TOWNS_PARENT.append(newFavoriteTown);
+         favoriteTowns.push(newFavoriteTown.firstChild.textContent);
+   } else {
+         if (favoriteTowns.includes(townName)) {
+          return;
+         } else {
+            const newFavoriteTown = document.createElement('div');
+            newFavoriteTown.classList = 'box__right__towns__item';
+            newFavoriteTown.innerHTML = `<div class="box__right__towns__item__name">${townName}</div>
+            <img src="srs/icons/remove-icon.svg" alt="" class="delete">`;
+            UI.TOWNS_PARENT.append(newFavoriteTown);
+            favoriteTowns.push(newFavoriteTown.firstChild.textContent);
+         }
+
+   }
+});
 
  function convertTemperature(temp) {
     return Math.round(temp - 273.15);
