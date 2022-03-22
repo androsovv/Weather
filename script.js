b0a51f82766b1da6f7e36dc6812cf8d1cffb6af1
homePC <vladimirandrosov13@gmail.com>
2022-03-22 23:42:35 +0300

    localstorage

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
- import {favoriteTowns, UI, month} from './view.js';
+ import {
+    favoriteTowns,
+    UI,
+    month
+ } from './view.js';
+ import {
+    saveInformation,
+    getInformation
+ } from './localStorage.js';
 
- function hideBoxLeftContent() {                                 // скрытие контента табов now, details, forecast
-    UI.TABS_CONTENT.forEach(item => {     
+ function hideBoxLeftContent() { // скрытие контента табов now, details, forecast
+    UI.TABS_CONTENT.forEach(item => {
        item.style.display = 'none';
     });
     UI.TABS_BTN.forEach(item => {
@@ -9,7 +17,7 @@
     });
  }
 
- function showBoxLeftContent(i) {                               //ф-ция показа контента табов
+ function showBoxLeftContent(i) { //ф-ция показа контента табов
     UI.TABS_CONTENT[i].style.display = 'block';
     UI.TABS_BTN[i].classList.add('active');
  }
@@ -19,7 +27,7 @@
  showBoxLeftContent(0);
 
 
- UI.TABS_PARENT.addEventListener('click', event => {                     // переключение табов через делегирование
+ UI.TABS_PARENT.addEventListener('click', event => { // переключение табов через делегирование
     const target = event.target;
 
     if (target.classList.contains('box__left__options__item')) {
@@ -33,7 +41,7 @@
 
  });
 
- UI.SEARCH_BTN.addEventListener('click', event => {                      // показ погоды по кнопке поиска
+ UI.SEARCH_BTN.addEventListener('click', event => { // показ погоды по кнопке поиска
     event.preventDefault();
     UI.FORECAST_PARENT.innerHTML = '';
     forecast(UI.SEARCH_TOWN.value);
@@ -44,10 +52,10 @@
 
     let weatherResponse = fetch(url);
     getWheather(weatherResponse);
-   
+
  });
 
- UI.ADD_FAVORITE.addEventListener('click', event => {                    //добавляем любимый город
+ UI.ADD_FAVORITE.addEventListener('click', event => { //добавляем любимый город
     event.preventDefault();
     let townName = document.querySelector('.town__name').textContent;
 
@@ -76,6 +84,8 @@
     favoriteTowns.push(newFavoriteTown.firstChild.textContent);
     deleteFavoriteTown();
     showFavoriteTownInfo();
+    saveInformation(favoriteTowns);
+
  }
 
  function deleteFavoriteTown() {
@@ -89,7 +99,7 @@
     });
  }
 
- function showFavoriteTownInfo() {                               //функция показа погоды по уже добавленному любимому городу
+ function showFavoriteTownInfo() { //функция показа погоды по уже добавленному любимому городу
     const showInfo = document.querySelectorAll('.box__right__towns__item__name');
     showInfo.forEach(item => {
        item.addEventListener('click', event => {
@@ -103,7 +113,7 @@
              url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
           let weatherResponse = fetch(url);
           forecast(item.textContent);
-          getWheather(weatherResponse); 
+          getWheather(weatherResponse);
 
        });
     });
@@ -150,25 +160,25 @@
  }
 
  function getWheather(response) {
-   response.then(getWheather => getWheather.json())
-   .then(result => {
-      const sunrise = new Date(result.sys.sunrise * 1000),
-         sunset = new Date(result.sys.sunset * 1000);
-      console.log(result, result.sys.sunrise);
-      UI.SEARCH_TOWN.value = '';
-      UI.MAIN_ICON.style.background = `url(https://openweathermap.org/img/wn/${result.weather[0].icon}.png) center center/cover no-repeat`;
-      UI.MAIN_ICON.style.backgroundSize = '80%';
-      UI.TEMPERATURE_NOW.textContent = convertTemperature(result.main.temp) + '°';
-      UI.TOWN_NAME.forEach(item => {
-         item.textContent = result.name;
-      });
-      UI.TEMPERATURE_DETAILS.textContent = 'Temperature: ' + convertTemperature(result.main.temp) + '°';
-      UI.FEELS_LIKE_DETAILS.textContent = 'Feels like: ' + convertTemperature(result.main.feels_like) + '°';
-      UI.WHEATHER_DETAILS.textContent = 'Wheather: ' + (result.weather[0].main);
-      UI.SUNRISE_DETAILS.textContent = 'Sunrise: ' + (sunrise.getHours()) + ':' + (sunrise.getMinutes());
-      UI.SUNSET_DETAILS.textContent = 'Sunset: ' + (sunset.getHours()) + ':' + (sunset.getMinutes());
-   })
+    response.then(getWheather => getWheather.json())
+       .then(result => {
+          const sunrise = new Date(result.sys.sunrise * 1000),
+             sunset = new Date(result.sys.sunset * 1000);
+          console.log(result, result.sys.sunrise);
+          UI.SEARCH_TOWN.value = '';
+          UI.MAIN_ICON.style.background = `url(https://openweathermap.org/img/wn/${result.weather[0].icon}.png) center center/cover no-repeat`;
+          UI.MAIN_ICON.style.backgroundSize = '80%';
+          UI.TEMPERATURE_NOW.textContent = convertTemperature(result.main.temp) + '°';
+          UI.TOWN_NAME.forEach(item => {
+             item.textContent = result.name;
+          });
+          UI.TEMPERATURE_DETAILS.textContent = 'Temperature: ' + convertTemperature(result.main.temp) + '°';
+          UI.FEELS_LIKE_DETAILS.textContent = 'Feels like: ' + convertTemperature(result.main.feels_like) + '°';
+          UI.WHEATHER_DETAILS.textContent = 'Wheather: ' + (result.weather[0].main);
+          UI.SUNRISE_DETAILS.textContent = 'Sunrise: ' + (sunrise.getHours()) + ':' + (sunrise.getMinutes());
+          UI.SUNSET_DETAILS.textContent = 'Sunset: ' + (sunset.getHours()) + ':' + (sunset.getMinutes());
+       })
 
 
-   .catch(error => alert(error));
+       .catch(error => alert(error));
  }
